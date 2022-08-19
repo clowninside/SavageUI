05b78565f29b1d2e8034c000d5fda9dae5e1ced9
clowninside <65174006+clowninside@users.noreply.github.com>
2022-08-19 16:44:46 +0300

    Update sign_page.dart

--- lib/pages/sign_page.dart
+++ lib/pages/sign_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:savage_ui/widgets/black_button.dart';
+import 'package:savage_ui/widgets/light_text_button.dart';
+import 'package:savage_ui/widgets/orange_button.dart';
 import '../widgets/top_bar.dart';
 
 class SignPage extends StatelessWidget {
@@ -9,27 +12,65 @@ class SignPage extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.white,
       body: Column(
-        children: const [
-          TopBar(),
-          SizedBox(
+        children: [
+          const TopBar(),
+          const SizedBox(
             height: 48,
           ),
-          Text('EMAIL'),
-          SizedBox(
+          Row(
+            children: [
+              Container(
+                margin: const EdgeInsets.only(left: 28, right: 18),
+                height: 38,
+                width: 38,
+                decoration: BoxDecoration(
+                  color: Colors.grey.shade900,
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: const Icon(
+                  Icons.mail_outline,
+                  color: Colors.white,
+                ),
+              ),
+              const Text('EMAIL'),
+            ],
+          ),
+          const SizedBox(
             height: 28,
           ),
-          Text('PASSWORD'),
-          SizedBox(
+          Row(
+            children: [
+              Container(
+                margin: const EdgeInsets.only(left: 28, right: 18),
+                height: 38,
+                width: 38,
+                decoration: BoxDecoration(
+                  color: Colors.grey.shade900,
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: const Icon(
+                  Icons.lock_outline,
+                  color: Colors.white,
+                ),
+              ),
+              const Text('PASSWORD'),
+            ],
+          ),
+          const SizedBox(
             height: 38,
           ),
-          Text('LOGIN'),
-          SizedBox(
+          const OrangeButton(
+            text: 'Login',
+          ),
+          const SizedBox(
             height: 18,
           ),
-          Text('Forgot password?'),
-          Spacer(),
-          Text('CREATE AN ACCOUNT'),
-          SizedBox(
+          const LightTextButton(text: 'Forgot password?'),
+          const Spacer(),
+          const BlackButton(
+            text: 'Create an account',
+          ),
+          const SizedBox(
             height: 55,
           ),
         ],
